cc75ffa6fe595c24aa38085211dabf03ba0061c8
Spencer Wood <spencer.wood@me.com>
2020-02-18 19:00:28 -0600

    added all the required js files to the app js file. Working on the override fuction currently

---
+++ app.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+
