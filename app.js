884e718c467703ff8512685322d32b13766cf90f
Spencer Wood <spencer.wood@me.com>
2020-02-18 22:00:09 -0600

    created the inquerie questions to create the objects, need to finish the .then sections

--- app.js
+++ app.js
@@ -4,3 +4,107 @@ const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 
+const team = [];
+
+function app(){
+
+    function getTeam(){
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'jobTitle',
+                message: 'What type of Team Member are we adding?',
+                choices: ['Manager', 'Intern', 'Engineer']
+            }
+
+        ]).then(userResponse => {
+            switch (userResponse.jobTitle){
+                //if Engineer is selected ask these questions to get info
+                case 'Engineer':
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'engineerName',
+                            message: 'Engineer Name:'
+                        },
+                        {
+                            type: 'input',
+                            name: 'engineerId',
+                            message: 'Engineer ID:'
+                        },
+                        {
+                            type: 'input',
+                            name: 'engineerEmail',
+                            message: 'Engineers Email:'
+                        },
+                        {
+                            type: 'input',
+                            name: 'engineerGithub',
+                            message: 'Engineers Github:'
+                        },
+                        //create the new engineer item, pass to array then go to getTeam() again
+                    ]).then();
+                    break;
+                    //if Manager is selected ask the questions to create manager object
+                case 'Manager':
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'managerName',
+                            message: 'Managers Name:'
+                        },
+                        {
+                            type: 'input',
+                            name: 'managerID',
+                            message: 'Managers ID:'
+                        },
+                        {
+                            type: 'input',
+                            name: 'managerEmail',
+                            message: 'Managers Email:'
+                        },
+                        {
+                            type: 'input',
+                            name: 'managerOffice',
+                            message: 'Office Number:'
+                        },
+                        //create the new maanger object then go back to getTeam()
+                    ]).then()
+                    break;
+                    //if Intern is selected ask questions to create the intern object
+                    case 'Intern':
+                        inquirer.prompt([
+                            {
+                                type: 'input',
+                                name: 'internName',
+                                message: 'Intern Name:',
+                            },
+                            {
+                                type: 'input',
+                                name: 'internID',
+                                message: 'Intern ID:'
+                            },
+                            {
+                                type: 'input',
+                                name: 'internEmail',
+                                message: 'Intern Email:'
+                            },
+                            {
+                                type: 'input',
+                                name: 'internSchool',
+                                message: 'Interns School:'
+                            },
+                            //create new intern object, add to array then go to getTeam()
+                        ]).then()
+                        break;
+                            //if nothing is selected go back to question
+                        default:
+                            getTeam();
+            };
+        });
+
+    };
+};
+//run the app to build the team
+app();
